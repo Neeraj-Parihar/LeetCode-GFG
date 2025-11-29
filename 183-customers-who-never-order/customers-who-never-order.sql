d99fcceccee34e6437b475fb3b974fe47a015e48
NEERAJ PARIHAR <125037303+Neeraj-Parihar@users.noreply.github.com>
2025-11-29 12:07:23 +0530

    Time: 202 ms (80.82%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,3 +1,3 @@
 -- Write your PostgreSQL query statement below
 
-select name As "Customers" from Customers where id NOT IN (select customerId from Orders);
+select name As "Customers" from Customers where id NOT IN (select customerId from Orders where customerId is NOT NULL);
